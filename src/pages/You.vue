53dfa0e2169151daab7138bbccc01d0d46d08e1f
jonafrank13 <jona.mailbox@gmail.com>
2021-02-24 01:14:52 +0530

    update website

--- src/pages/You.vue
+++ src/pages/You.vue
@@ -4,10 +4,10 @@
       <h4 class="full-width text-accent text-bold q-mt-sm q-mb-lg">About You</h4>
       <template v-if="userData.region">
         <p>
-          <span>Hi , It seems like you are accessing the my website from a the ip address 📡 <i>{{userData.query}}</i>, your internet service provider is <i>{{userData.isp}}</i></span><br>
+          <span>Hi , It seems like you are accessing the my website from a the ip address 📡 <i>{{userData.ip}}</i>, your internet service provider is <i>{{userData.org}}</i></span><br>
           <span>I see that you are using a <i>{{userData.device.brand}}&nbsp;{{userData.device.type}}</i>&nbsp;💻&nbsp;specifically a <i>{{userData.client.name}}</i> browser 🌐 , which is on version 🚥<i>{{userData.client.version}}</i></span><br>
           <span>Your OS 💿 seems to be <i>{{userData.os.name}}</i>, which is on version 🚥<i>{{userData.os.version}}</i>&nbsp;<span v-if="userData.memory">having at least {{userData.memory}} GB of RAM.</span></span><br>
-          <span>I sense that you are currently at &nbsp;🌎<i>{{userData.lat}}&#176;</i> latitude &amp; <i>{{userData.lon}}&#176;</i> longitude, which is around 🗺️<i>{{userData.city}},</i>&nbsp;<i>{{userData.regionName}},</i>&nbsp;<i>{{userData.country}}</i><span v-if="userData.zip"> with the zipcode <i>{{userData.zip}}</i></span></span><br>
+          <span>I sense that you are currently at &nbsp;🌎<i>{{userData.latitude}}&#176;</i> latitude &amp; <i>{{userData.longitude}}&#176;</i> longitude, which is around 🗺️<i>{{userData.city}},</i>&nbsp;<i>{{userData.region}},</i>&nbsp;<i>{{userData.country_name}}</i><span v-if="userData.postal"> with the zipcode <i>{{userData.postal}}</i></span></span><br>
         </p>
         <p v-if="userData.orientation">
           Your device's current orientation is x:&nbsp;<i>{{userData.orientation.alpha}}</i>&nbsp;y:&nbsp;<i>{{userData.orientation.beta}}</i>&nbsp;z:&nbsp;<i>{{userData.orientation.gamma}}</i>
@@ -40,12 +40,13 @@ export default {
     const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
     const connection = navigator.connection
     const memory = navigator.deviceMemory
-    fetch('http://ip-api.com/json').then(res => res.json()).then(data => {
+    fetch('https://ipapi.co/json/').then(res => res.json()).then(data => {
       this.userData = { ...data, ...device, connection, memory, darkMode: isDark }
       window.addEventListener('deviceorientation', this.handleOrientation, true)
       window.navigator.getBattery().then(data => {
         this.$set(this.userData, 'battery', data)
       })
+      console.log(this.userData)
     })
   },
   methods: {
