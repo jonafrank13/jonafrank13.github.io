1e6736602749b6a22a9de0acbca97f1abd796af2
Jona Frank <jonafrank@jonafrankmacbookpro.local>
2021-05-21 02:18:56 +0530

    fix: updates

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,30 @@
 <template>
   <div id="q-app">
     <router-view />
+    <custom-cursor
+    :targets="['img', 'a', 'button', 'i', 'svg']"
+    :circleColor="'#5dc855'"
+    :circleColorHover="'#fff'"
+    :dotColor="'#ed3833'"
+    :dotColorHover="'#5dc855'"
+    :hoverSize="1.8"
+  ></custom-cursor>
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
+import CustomCursor from 'components/CustomCursor'
+
 Vue.prototype.windowObj = window
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    CustomCursor
+  }
 }
 </script>
+<style>
+#q-app { cursor: none }
+aside { cursor: auto }
+</style>
